fdab5dc6728bc30afe4fefb4a30e9126df404973
Julie Davison <davison.jules@gmail.com>
2019-09-28 18:25:35 -0700

    changed mongoose.connect

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 
-// mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI);
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
